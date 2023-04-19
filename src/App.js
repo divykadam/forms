9a754e504868c6474494a57125e8f8afd191f2bb
DivyansheeKadam <divyansheekadam6@gmail.com>
2023-04-19 15:37:12 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Routing/Layout";
 import { FormClass } from "./Form/classComponent/FormClass";
 import EmpData from "./Form/classComponent/EmpData";
@@ -6,21 +7,58 @@ import EmpEdit from "./Form/classComponent/EmpEdit";
 import FunctionEditData from "./Form/functionComponent/FunctionEditData";
 import FunctionForm from "./Form/functionComponent/FunctionForm";
 import ShowFunction from "./Form/functionComponent/ShowFunction";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "formC",
+        element: <FormClass />,
+      },
+      {
+        path: "showAllC",
+        element: <EmpData />,
+      },
+      {
+        path: "editC",
+        element: <EmpEdit />,
+      },
+
+      // Function From Routing
+      {
+        path: "form",
+        element: <FunctionForm />,
+      },
+      {
+        path: "showAll",
+        element: <ShowFunction />,
+      },
+      {
+        path: "edit/:id",
+        element: <FunctionEditData />,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
     <div className="App">
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="formC" element={<FormClass />} />
           <Route path="showAllC" element={<EmpData />} />
-          <Route path="editC" element={<EmpEdit />} />
+          <Route path="editC" element={<EmpEdit />} /> */}
 
-          {/* Function From Routing */}
-          <Route path="form" element={<FunctionForm />} />
+      {/* Function From Routing */}
+      {/* <Route path="form" element={<FunctionForm />} />
           <Route path="showAll" element={<ShowFunction />} />
           <Route path="edit/:id" element={<FunctionEditData />} />
         </Route>
-      </Routes>
+      </Routes> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./Redux/Store";
-import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </React.StrictMode>
   </Provider>
 );
