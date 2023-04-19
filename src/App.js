56947c50e3d415e5d944a5ac655c4d968dccb1a6
DivyansheeKadam <divyansheekadam6@gmail.com>
2023-04-19 16:03:27 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import { Route, Routes } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Routing/Layout";
 import { FormClass } from "./Form/classComponent/FormClass";
@@ -7,6 +6,7 @@ import EmpEdit from "./Form/classComponent/EmpEdit";
 import FunctionEditData from "./Form/functionComponent/FunctionEditData";
 import FunctionForm from "./Form/functionComponent/FunctionForm";
 import ShowFunction from "./Form/functionComponent/ShowFunction";
+import Error from "./Form/Error";
 
 const router = createBrowserRouter([
   {
@@ -39,6 +39,10 @@ const router = createBrowserRouter([
         path: "edit/:id",
         element: <FunctionEditData />,
       },
+      {
+        path: "*",
+        element: <Error />,
+      },
     ],
   },
 ]);
@@ -46,18 +50,6 @@ const router = createBrowserRouter([
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="formC" element={<FormClass />} />
-          <Route path="showAllC" element={<EmpData />} />
-          <Route path="editC" element={<EmpEdit />} /> */}
-
-      {/* Function From Routing */}
-      {/* <Route path="form" element={<FunctionForm />} />
-          <Route path="showAll" element={<ShowFunction />} />
-          <Route path="edit/:id" element={<FunctionEditData />} />
-        </Route>
-      </Routes> */}
       <RouterProvider router={router} />
     </div>
   );
